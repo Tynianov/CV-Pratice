d25b9fa189635fad84ee79a92477c050fda18e17
cr0manty <denis.dudka@faifly.com>
2020-05-27 00:44:17 +0300

    try to fix iris recognition

--- cv_project/iris/views.py
+++ cv_project/iris/views.py
@@ -24,19 +24,16 @@ class IrisRecognizeAPIView(APIView):
             rgb = cv2.cvtColor(encoded_image, cv2.COLOR_BGR2RGB)
             code, mask = encode_photo(rgb)
 
-            result = {}
-            percentage = 0
+            result = []
             for iris in PersonIris.objects.all():
                 recognize = iris.compare_iris(code, mask)
-                if recognize and recognize['percentage'] > percentage:
-                    result = recognize
-                    percentage = result['percentage']
+                if recognize and recognize['percentage'] > 0.6:
+                    result.append(recognize)
 
-            LogEntry.objects.create(authorization_type=LogEntry.IRIS, result=result is not None,
-                                    person=result.get('person_data'), percentage=percentage, image=image)
+            # LogEntry.objects.create(authorization_type=LogEntry.IRIS, result=result is not None,
+            #                         person=result.get('person_data'), percentage=percentage, image=image)
 
             if result:
-                result.pop('person_data')
                 return Response(result, status=200)
             return Response('Not Found', status=404)
         except Exception as e:
